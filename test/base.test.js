85382c738b2e25049e1b1a0936823c56965f1363
Ulpian Morina <ulpianmorina@gmail.com>
2016-11-09 09:53:44 +0000

    added tests

---
+++ test/base.test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const config = require('config');
+const api = require('../in')(config);
+
+const mocha = require('mocha');
+const expect = require('chai').expect;
+const request = require('supertest');
+
+// Supertest connection to api
+const tapi = request(api.listener);
+
+describe('/', () => {
+    it('Should get root endpoint', (done) => {
+        tapi.get('/')
+        .expect(200, (err, resp) => {
+
+            expect(resp.text).to.be.a('string');
+            expect(resp.text).to.equal('hey world!');
+
+            done();
+        });
+    })
+})
